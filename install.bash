9ba136fc420fe6531284a8428fd42e85a463d584
Thomas Polasek <polast@polast.spgear.lab.emc.com>
2013-08-29 11:50:33 -0600

    improved install script

--- install.bash
+++ install.bash
@@ -1,8 +1,23 @@
 #!/bin/bash
-echo "=======Copying======"
-cp -R ./bin ~/bin
-cp ./.* ~/.
-echo "=======Results======"
-ls -la ~
-ls -la ~/bin/*
 
+#NOTE: run this script from the dotfiles directory
+
+ROOT=/root
+
+echo "=======Copying config files======"
+
+#install for current user
+cp -R ./bin ~/bin
+cp -R ./.vim ~/.vim
+cp ./.* ~/.
+
+#install for root
+sudo cp -R ./bin "${ROOT}/bin"
+sudo cp -R ./.vim "${ROOT}/.vim"
+sudo cp -R ./.* "${ROOT}"
+
+echo "=====updating apt and upgrading programs===="
+sudo apt-get update && sudo apt-get upgrade
+
+echo "=====installing essential packages====="
+sudo apt-get install meld build-essential git tmux vim vim-gnome libncurses5-dev  
